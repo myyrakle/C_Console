131c3a31f45d5152b9a513e1cddbe9546938144e
myyrakle <sssang97@naver.com>
2019-12-25 17:36:29 +0900

    Add files via upload

--- source/Console.c
+++ source/Console.c
@@ -4,14 +4,19 @@ void __Console_Write(const char*);
 void __Console_WriteLine(const char*);
 //void __Console_WriteFormat(const char*, ...);
 
+void __Console_FlushOut();
+
 //int __Console_Read(const char*);
 void __Console_ReadLine(char*, size_t);
 //void(*ReadFormat)(const char*, ...);
+
+void __Console_FlushIn();
 
 void __Console_SetTextColor(Color);
 Color __Console_GetTextColor();
 void __Console_SetBackgroundColor(Color);
 Color __Console_GetBackgroundColor();
+void __Console_ResetColor();
 
 void __Console_Clear();
 void __Console_Pause();
@@ -19,19 +24,24 @@ void __Console_Exit();
 
 struct __Console Console = {
    .__color = BLACK,
-   
+
    .Write = __Console_Write,
    .WriteLine = __Console_WriteLine,
    .WriteFormat = printf,
+
+   .FlushOut = __Console_FlushOut,
 
    .Read = getchar,
    .ReadLine = __Console_ReadLine,
    .ReadFormat = scanf,
 
+   .FlushIn = __Console_FlushIn,
+
    .SetTextColor = __Console_SetTextColor,
    .GetTextColor = __Console_GetTextColor,
    .SetBackgroundColor = __Console_SetBackgroundColor,
    .GetBackgroundColor = __Console_SetBackgroundColor,
+   .ResetColor = __Console_ResetColor,
 
    .Clear = __Console_Clear,
    .Pause = __Console_Pause,
@@ -49,14 +59,29 @@ void __Console_WriteLine(const char* text)
 	putchar('\n');
 }
 
+void __Console_FlushOut()
+{
+	fflush(stdout);
+}
+
 void __Console_ReadLine(char* buffer, size_t max)
 {
 	fgets(buffer, max, stdin);
 }
 
+void __Console_FlushIn()
+{
+	int c;
+	while( 
+		(c = getchar()) != '\n' 
+		&& 
+		c != EOF 
+	);
+}
+
 void __Console_SetTextColor(Color color)
 {
-	Console.__color |= color & 0xf;
+	*(Color*)&Console.__color |= color & 0xf;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), Console.__color);
 }
 
@@ -67,13 +92,19 @@ Color __Console_GetTextColor()
 
 void __Console_SetBackgroundColor(Color color)
 {
-	Console.__color |= (color&0xf)<<4;
+	*(Color*)& Console.__color |= (color & 0xf) << 4;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), Console.__color);
 }
 
 Color __Console_GetBackgroundColor()
 {
 	return (Console.__color & 0xff) >> 4;
+}
+
+void __Console_ResetColor()
+{
+	*(Color*)& Console.__color = TextDefault | (BackgroundDefault << 4);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), Console.__color);
 }
 
 void __Console_Clear()
